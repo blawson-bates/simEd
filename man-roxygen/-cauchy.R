9ecb389dc0ada316f6511195f15fd3b732a26898
Barry Lawson <blawson@bates.edu>
2023-11-21 11:34:43 -0500

    Updated various roxygen templates to remove Unicode math chars

--- man-roxygen/-cauchy.R
+++ man-roxygen/-cauchy.R
@@ -5,7 +5,7 @@
 #'     The Cauchy distribution has density
 #'     \deqn{f(x) = \frac{1}{\pi s} \ \left(1 + \left( \frac{x - l}{s} \right)^2
 #'               \right)^{-1}}{%
-#'           f(x) = f(x) = 1 / (π s (1 + ((x-l)/s)^2))}
+#'           f(x) = 1 / (\pi s (1 + ((x-l)/s)^2))}
 #'   for all \eqn{x}.
 #'
 #'   The mean is \eqn{a/(a+b)} and the variance is \eqn{ab/((a+b)^2 (a+b+1))}.
--- man-roxygen/-pois.R
+++ man-roxygen/-pois.R
@@ -3,8 +3,8 @@
 #' @details
 #'  The Poisson distribution has density
 #'      \deqn{p(x) = \frac{\lambda^x e^{-\lambda}}{x!}}{%
-#'            p(x) = λ^x exp(-λ)/x!}
+#'            p(x) = \lambda^x exp(-\lambda)/x!}
 #'  for \eqn{x = 0, 1, 2, \ldots}.
-#'  The mean and variance are \eqn{E(X) = Var(X) = \lambda}{E(X) = Var(X) = λ}
+#'  The mean and variance are \eqn{E(X) = Var(X) = \lambda}{E(X) = Var(X) = \lambda}
 #'
 #' @seealso \code{\link[=rpois]{stats::rpois}}
